2d8630d1cbdc2de2765e3425089c51b5680550ad
Luke-McDougall <lukeyr10@gmail.com>
2019-09-25 05:08:24 +0800

    basic interface beginning

--- src/file_explorer.c
+++ src/file_explorer.c
@@ -1,35 +1,82 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <stdio.h>
-//#include "../include/termbox.h"
+#include <stdlib.h>
+#include "../include/termbox.h"
 #include "../include/strings.h"
+#include "../include/types.h"
+
+typedef struct 
+{
+    u32 current_line;
+    u32 num_lines;
+    u32 view_range_start;
+    // view_range_end is one more than the last line with visible text
+    u32 view_range_end;
+
+    String **buffer;
+} Buffer;
+
+void update_screen(Buffer *screen)
+{
+    struct tb_cell *tb_buffer = tb_cell_buffer();
+    for(u32 y = screen->view_range_start; y < screen->num_lines; y++)
+    {
+        String *line = screen->buffer[y];
+        for(u32 x = 0; x < line->length; x++)
+        {
+            u32 tb_index = x + tb_width() * y;
+            tb_buffer[tb_index].ch = (u32)line->start[x];
+            tb_buffer[tb_index].fg = TB_WHITE;
+            tb_buffer[tb_index].bg = y == screen->current_line ? TB_BLUE : TB_BLACK;
+        }
+    }
+    tb_present();
+}
 
 int main()
 {
-    //tb_init();
-    //struct tb_event event = {};
+    tb_init();
+    struct tb_event event = {};
     struct dirent *dir;
     char path[128];
     size_t size = 128;
     getcwd(path, size);
-    String *real_path = string_from(path);
-    String *real_path_copy = string_copy(real_path);
-    String *files[10];
-    int index = 0;
     DIR *cwd = opendir(path);
+    Buffer screen = {};
+    screen.buffer = (String**)malloc(sizeof(String*) * 100);
+    screen.view_range_end = tb_height() + 1;
+    u32 index = 0;
     while(dir = readdir(cwd))
     {
-        if(dir->d_type == DT_REG)
+        screen.buffer[index++] = string_from(dir->d_name); 
+        screen.num_lines++;
+    }
+
+    for(;;)
+    {
+        update_screen(&screen);
+        tb_poll_event(&event);
+        if((u8)event.ch == 'j')
         {
-            files[index] = string_from(dir->d_name);
-            index++;
+            screen.current_line = (screen.current_line + 1) % screen.num_lines;
+        }
+        else if((u8)event.ch == 'k')
+        {
+            if(screen.current_line == 0)
+            {
+                screen.current_line = screen.num_lines - 1;
+            }
+            else
+            {
+                screen.current_line--;
+            }
+        }
+        else
+        {
+            break;
         }
     }
-    printf("%s\n", real_path_copy->start);
-    for(int i = 0; i < index; i++)
-    {
-        printf("%s, ", files[i]->start);
-    }
-    printf("\n");
+    tb_shutdown();
     return 0;
 }
