7514001f782238575cfd0fbe63a87fa121778369
Luke McDougall <lukeyr10@gmail.com>
2020-01-24 08:19:16 +0800

    minor changes

---
+++ src/config.h
@@ -0,0 +1,23 @@
+/*
+ * Here you can change key bindings. The default should be familiar
+ * to vim users.
+ */
+
+unsigned char MENU_DOWN = 'j';
+unsigned char MENU_UP = 'k';
+unsigned char MENU_LEFT = 'h';
+unsigned char MENU_RIGHT = 'l';
+
+unsigned char DELETE_FILE = 'D';
+unsigned char CUT_FILE = 'd';
+unsigned char COPY_FILE = 'y';
+unsigned char PASTE_FILE = 'p';
+
+unsigned char ENTER_SEARCH = '/';
+unsigned char ENTER_INSERT = 'i';
+unsigned char ENTER_VISUAL = 'v';
+unsigned char CHANGE_ACTIVE_WINDOW = 'w';
+unsigned char VERTICAL_SPLIT = 'V';
+
+unsigned char JUMP_TO_START = 'g';
+unsigned char JUMP_TO_END = 'G';
